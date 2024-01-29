a3b9528aad8a2ff4afe0abefcea0b6827668262b
Chaeyeon Seo <120750451+ChaeYami@users.noreply.github.com>
2024-01-30 04:05:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ markdown_text = """
 <br>
 
 ![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ChaeYami&layout=compact&theme=material-palenight)
-<br>
-
 ![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=ChaeYami&show_icons=true&theme=material-palenight)&nbsp;
 <br>  
 
