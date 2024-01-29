869912585b02982c786b11433181904bed1b6f2b
Chaeyeon Seo <120750451+ChaeYami@users.noreply.github.com>
2024-01-30 03:50:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,54 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST=5
 
 markdown_text = """
+<div align="center"><img src="https://capsule-render.vercel.app/api?type=waving&color=0:41d1fe,100:fe84fe&height=250&section=header&text=SEO%20CHAEYEON&fontColor=ffffffCC&fontSize=60&fontAlignY=35&desc=ChaeYami&descSize=20&descAlign=70&descAlignY=53" /></div>
+<div align="center"><img src="https://github.com/ChaeYami/ChaeYami/assets/120750451/7c8742a2-96f5-4f80-948f-fc5fc8afdcd2" width="160"/></div>
+
+<br>  
+
+# 🛠️ SKILLS
+### Languages & Frameworks
+<img src="https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=white"> <img src="https://img.shields.io/badge/java-007396?style=for-the-badge&logo=java&logoColor=white">
+<br>
+<img src="https://img.shields.io/badge/django-092E20?style=for-the-badge&logo=django&logoColor=white">
+<img src="https://img.shields.io/badge/spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white">
+<img src="https://img.shields.io/badge/spring%20boot-6DB33F?style=for-the-badge&logo=springboot&logoColor=white">
+<br>
+<img src="https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+<img src="https://img.shields.io/badge/css-1572B6?style=for-the-badge&logo=css3&logoColor=white">
+<img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"> 
+### Tools
+<img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white"> <img src="https://img.shields.io/badge/git-F05032?style=for-the-badge&logo=git&logoColor=white">
+<img src ="https://img.shields.io/badge/Visual%20Studio%20Code-007ACC.svg?&style=for-the-badge&logo=Visual%20Studio%20Code&logoColor=white">
+<img src ="https://img.shields.io/badge/intellij-000000.svg?&style=for-the-badge&logo=intellijidea&logoColor=white">
+<br>
+<img src ="https://img.shields.io/badge/notion-000000.svg?&style=for-the-badge&logo=notion&logoColor=white">
+<img src ="https://img.shields.io/badge/obsidian-7C3AED.svg?&style=for-the-badge&logo=obsidian&logoColor=white">
+
+<br>
+
+# 📬 CONTACT
+<a href="https://chaeyami.tistory.com/" target="_blank"><img src="https://img.shields.io/badge/Tistory-FF4785?style=for-the-badge&logo=tistory&logoColor=000000"></a>
+[![Gmail Badge](https://img.shields.io/badge/Gmail-d14836?style=for-the-badge&logo=Gmail&logoColor=white&link=mailto:ssallyseo@gmail.com)](mailto:ssallyseo@gmail.com)
+
+<br>
+
+---
+
+<br>
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ChaeYami&layout=compact&theme=material-palenight)
+<br>
+
+![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=ChaeYami&show_icons=true&theme=material-palenight)&nbsp;
+<br>  
+
+[![Solved.ac프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=codusseo)](https://solved.ac/codusseo)
+<br>  
+
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FChaeYami&count_bg=%23FF9EDE&title_bg=%23E1A3FF&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true)](https://hits.seeyoufarm.com)
+<br>
+
 
 
 """ # list of blog posts will be appended here
